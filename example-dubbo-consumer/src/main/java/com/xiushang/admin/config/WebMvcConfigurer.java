53c8f2377cef408266240a9a06652708217450a7
liukefu2050 <liukefu2050@sina.com>
2022-08-15 17:34:15 +0800

    init

--- example-dubbo-consumer/src/main/java/com/xiushang/admin/config/WebMvcConfigurer.java
+++ example-dubbo-consumer/src/main/java/com/xiushang/admin/config/WebMvcConfigurer.java
@@ -4,11 +4,10 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.xiushang.common.json.FastJsonMappingHttpMessageConverter;
 import com.xiushang.config.CustomRequestMappingHandlerMapping;
-import com.xiushang.validation.utils.ValidationConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.nio.charset.StandardCharsets;
@@ -48,46 +47,6 @@ public class WebMvcConfigurer extends WebMvcConfigurationSupport {
         converter.setDefaultCharset(StandardCharsets.UTF_8);
         converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
         converters.add(converter);
-    }
-
-
-    /**
-     * 页面跨域访问Controller过滤
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
-        registry.addMapping("/**")
-                .allowedHeaders("*")
-                .allowedMethods("POST", "GET")//, "PUT", "DELETE"
-                .allowedOrigins("*");
-    }
-
-
-    /**
-     * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。 需要重新指定静态资源
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/","classpath:/templates/");
-
-        registry.addResourceHandler("doc.html").addResourceLocations(
-                "classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations(
-                "classpath:/META-INF/resources/webjars/");
-        // registry.addResourceHandler(fileUploadProperties.getAccessUrl())
-        //        .addResourceLocations("file:" + fileUploadProperties.getPath());
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        //super.addViewControllers(registry);
-        //registry.addViewController("/nihao").setViewName("success");
-
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index.html").setViewName("index");
     }
 
 }
--- example-dubbo-consumer/src/main/java/com/xiushang/admin/controller/ConsumerController.java
+++ example-dubbo-consumer/src/main/java/com/xiushang/admin/controller/ConsumerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/test")
 public class ConsumerController {
-    @DubboReference
+    @DubboReference(check = false)
     OrderPayDubboService orderPayDubboService;
 
     @GetMapping("list")
