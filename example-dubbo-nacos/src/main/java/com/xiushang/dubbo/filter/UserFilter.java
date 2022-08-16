3daf5ebee7fd24355279027db0952bbb695156f5
liukefu2050 <liukefu2050@sina.com>
2022-08-16 11:16:07 +0800

    init

--- example-dubbo-nacos/src/main/java/com/xiushang/dubbo/filter/UserFilter.java
+++ example-dubbo-nacos/src/main/java/com/xiushang/dubbo/filter/UserFilter.java
@@ -1,12 +1,9 @@
 package com.xiushang.dubbo.filter;
 
 
-import com.xiushang.framework.utils.UserHolder;
-import com.xiushang.security.SecurityUser;
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
-import com.xiushang.framework.log.*;
 import org.apache.dubbo.rpc.cluster.filter.ClusterFilter;
 
 @Activate(group = CommonConstants.CONSUMER)
@@ -18,18 +15,13 @@ public class UserFilter implements ClusterFilter {
         //RpcContext.getServerAttachment().setAttachment("getServerAttachment", "1");
         //System.out.println(invoker.getInterface().getName() + "=========filter");
 
-        /*SecurityUser user = UserHolder.get();
+        //SecurityUser user = UserHolder.get();
         // dubbo 上下文中 找USER对象
-        if(user==null){
-            Object userObj =  RpcContext.getClientAttachment().getObjectAttachment(SecurityConstants.USER_KEY);
-            if (userObj instanceof SecurityUser) {
-                user = (SecurityUser) userObj;
-            }
-        }
+        Object user =  RpcContext.getClientAttachment().getObjectAttachment("SecurityConstants.USER_KEY");
 
         if(user!=null){
-            RpcContext.getClientAttachment().setObjectAttachment(SecurityConstants.USER_KEY,user);
-        }*/
+            RpcContext.getClientAttachment().setObjectAttachment("SecurityConstants.USER_KEY",user);
+        }
 
         Result result = invoker.invoke(invocation);
 
